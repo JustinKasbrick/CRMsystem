a6ccabd4675dc97bf31195597805ef4938a19c8a
Justin Kasbrick <justinkasbrick@gmail.com>
2020-07-06 07:54:15 -0600

    Added unit tests for User and Company classes.

---
+++ CRMsystem.Tests/Domain/CompanyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CRMsystem.Tests.Domain
+{
+    public class CompanyTests
+    {
+        [InlineData("mycorp.com", "email@mycorp.com", true)]
+        [InlineData("mycorp.com", "email@gmail.com", false)]
+        [Theory]
+        public void Differentiates_a_corporate_email_from_non_corporate(string domain, string email, bool expectedResult)
+        {
+            //arrange
+            var sut = new Company(domain, 0);
+
+            // act
+            var isEmailCorporate = sut.IsEmailCorporate(email);
+
+            // assert
+            Assert.Equal(expectedResult, isEmailCorporate);
+        }
+    }
+}
---
+++ CRMsystem.Tests/Domain/UserTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CRMsystem.Tests.Domain
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Changing_email_from_non_corporate_to_corporate()
+        {
+            var company = new Company("mycorp.com", 1);
+            var sut = new User(1, "user@gmail.com", UserType.Customer);
+
+            sut.ChangeEmail("new@mycorp.com", company);
+
+            Assert.Equal(2, company.NumberOfEmployees);
+            Assert.Equal("new@mycorp.com", sut.Email);
+            Assert.Equal(UserType.Employee, sut.Type);
+        }
+
+        [Fact]
+        public void Changing_email_from_corporate_to_non_corporate()
+        {
+            var company = new Company("mycorp.com", 1);
+            var sut = new User(1, "user@mycorp.com", UserType.Customer);
+
+            sut.ChangeEmail("new@gmail.com", company);
+
+            Assert.Equal(2, company.NumberOfEmployees);
+            Assert.Equal("new@gmail.com", sut.Email);
+            Assert.Equal(UserType.Customer, sut.Type);
+        }
+
+        [Fact]
+        public void Changing_email_to_the_same_one()
+        {
+            var company = new Company("mycorp.com", 1);
+            var sut = new User(1, "user@mycorp.com", UserType.Customer);
+
+            sut.ChangeEmail("user@mycorp.com", company);
+
+            Assert.Equal(1, company.NumberOfEmployees);
+            Assert.Equal("new@gmail.com", sut.Email);
+            Assert.Equal(UserType.Employee, sut.Type);
+        }
+    }
+}
